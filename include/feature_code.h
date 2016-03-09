39dde3bc94246e3c1af90cb955474408a8960955
Tori <vedwards@colby.edu>
2016-03-08 21:24:19 -0500

    added preliminary features

--- include/feature_code.h
+++ include/feature_code.h
@@ -1,15 +1,25 @@
 #ifndef FEATURE_CODE_H
 
+#include "opencv2/opencv.hpp"
+
 #define FEATURE_CODE_H
 
 
-// Ideas:
 //  Size of bounding box
+int bbArea( cv::Mat &boundingBox);
+
 //  width to height ratio
+float width2Height( cv::Mat &boundingBox);
+
 //  fill ratio
+float fillRatio( cv::Mat &boundingBox, cv::Mat &regMap);
+
+//  moments: central, primary axes**, 
+cv::Mat primaryAxis(cv::Mat &boundingBox, cv::Mat &centroid, cv::Mat &regMap);
+
 //  harris features
 //  shi-tomasi features
-//  moments: central, primary axes**, 
-//  pg. 22 of Bruce notes
+
+float *getFeatures(cv::Mat boundingBox, cv::Mat regionMap);
 
 #endif
