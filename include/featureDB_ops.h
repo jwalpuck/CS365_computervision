19a80032f6436353fd28b5cbf574007d54dcc5df
Jack Walpuck <jwalpuck@colby.edu>
2016-03-16 15:49:13 -0400

    started adding feature scoring

--- include/featureDB_ops.h
+++ include/featureDB_ops.h
@@ -4,8 +4,18 @@
 
 #define FEATUREDB_OPS_H
 
+const int EUC_DIST = 81;
+const int K_NEIGH = 10;
+
+inline float i_mean(float x, float y);
+inline float i_stddev(float x, float y);
+
 void writeFeatureToFile( ObjectFeature *feature, char *fileOutName );
 
 ObjectFeature *findBestFeatureResult( ObjectFeature *feature, char *fileInName ); 
+
+float scoreFeatures(ObjectFeature *cur, ObjectFeature *other, int distanceMetric);
+
+float scoreEuclidean(ObjectFeature *cur, ObjectFeature *other);
 
 #endif
